0684c8e67be5930e48a3eb0f8204344c5623f5e8
Jamy Golden <jamy@22seven.com>
2015-04-20 14:18:02 +0200

    neatened up comments slightly

--- js/jquery.plusanchor.js
+++ js/jquery.plusanchor.js
@@ -84,12 +84,12 @@
     };
 
     $.plusAnchor.defaults = {
-        easing: 'swing',      // String: Anything other than "swing" or "linear" requires the easing.js plugin
-        offsetTop: 0,         // Int: Top offset for anchor scrollto position. Can be positive or negative
-        speed: 1000,          // Int: The speed, in miliseconds, it takes to complete a slide
-        onInit: null,         // Function: Callback function on plugin initialize
-        onSlide: null,        // Function: Callback function that runs just before the page starts animating
-        performance: false    // Boolean: Toggles between click and delegate events.
+        easing: 'swing',   // String: Anything other than "swing" or "linear" requires the easing.js plugin
+        offsetTop: 0,      // Int: Top offset for anchor scrollto position. Can be positive or negative
+        speed: 1000,       // Int: The speed, in miliseconds, it takes to complete a slide
+        onInit: null,      // Function: Callback function on plugin initialize
+        onSlide: null,     // Function: Callback function that runs just before the page starts animating
+        performance: false // Boolean: Toggles between click and delegate events.
     };
 
     $.fn.plusAnchor = function(options){
